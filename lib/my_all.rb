40135441568942c069faadb7c05ad35fb0137f2b
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-18 12:19:09 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def my_all?(collection)
-  all_odd = [1,3].all? do |number|
-    number.odd?
+  i = 0
+  while i < collection.length
+    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
-  all_odd
 end
