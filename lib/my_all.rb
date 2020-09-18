a6e05a619c35e446b25e9ffb270fbee46d5bf8dc
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-18 12:15:40 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,6 +4,9 @@ def my_all?(collection)
   all_odd = [1,3].all? do |number|
     number.odd?
   end
-  all_odd 
-
+  all_odd
 end
+
+[4, 8, 9, 984].any?{|i| i < 9842}
+
+[1,2,3].detect{|i| i.odd?} #=> 1
